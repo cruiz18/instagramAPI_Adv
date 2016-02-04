2a9070c6babc33e7c7f875210d4a4e92d8402a41
cruiz18 <caruiz@cristoreyny.org>
2016-02-04 13:49:17 +0000

    Flickr project

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,6 +1,27 @@
 // YOUR CODE GOES HERE
-var clientID = "REPLACE ME WITH CLIENT ID";
+var clientID = "1ce17112b850e601f2181770fd4c0db2";
 
 $(document).ready(function() {
     // your code here
+    $("#search-btn").click(function() {
+        var input = $("#search-query").val();
+
+        searchFlickr(clientID, input, 10, function(photos) {
+            for (var i = 0; i< photos.photo.length; i++) {
+
+                $("#search-results").append( "<img src=" + photos.photo[i].url_m + ">");
+
+              
+                //select our list
+            }
+        });
+    });
+
+
+    $("#reset-btn").click(function() {
+        $("#search-results").empty();
+    });
+
 });
+
+//continue to add the pictures
